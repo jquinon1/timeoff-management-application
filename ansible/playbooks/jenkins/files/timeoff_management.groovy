2f48f4f87a229bf8adccd915a433901617df3e09
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2020-08-09 16:36:59 -0500

    Added an env var to the CI Job

--- ansible/playbooks/jenkins/files/timeoff_management.groovy
+++ ansible/playbooks/jenkins/files/timeoff_management.groovy
@@ -1,8 +1,13 @@
 def application_name = "timeoff-management"
 def application_repository = "git@github.com:jquinon1/timeoff-management-application.git"
 def application_credentials_id = "ssh_key"
+def registry_addr = "http://192.168.50.20:5000"
 pipelineJob("${application_name}-CI"){
   description "Continous integration job for ${application_name} app"
+  environmentVariables {
+    env('REGISTRY_ADDR', registry_addr)
+    keepBuildVariables(true)
+  }
   triggers {
     scm('* * * * *') {
       ignorePostCommitHooks()
